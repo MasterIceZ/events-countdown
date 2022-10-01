825780197cba4be82bfa8ae32c9819348227da86
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2022-10-01 11:32:01 +0700

    fix expiration

--- src/components/const/now.ts
+++ src/components/const/now.ts
@@ -27,4 +27,23 @@ function find_now() {
 	};
 }
 
+function find_next() {
+	var l = 0,
+		r = sorted.length - 1;
+	while (l < r) {
+		var mid = (l + r) >> 1;
+		if (isBefore(sorted[mid])) {
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+	r = r + 1;
+	return {
+		date: create_date(sorted[r]),
+		name: sorted[r].name,
+	};
+}
+
 export const current: any = find_now();
+export const next_time: any = find_next();
